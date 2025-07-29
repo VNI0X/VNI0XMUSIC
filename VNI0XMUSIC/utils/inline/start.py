1d4db7f48e161ae425d150eaa950e135c4a1171b
𝖲potify 𝖳ech <sachinxmeena@gmail.com>
2025-07-29 16:46:06 +0530

    Anon Music

--- VNI0XMUSIC/utils/inline/start.py
+++ VNI0XMUSIC/utils/inline/start.py
@@ -19,13 +19,9 @@ def start_panel(_):
 def private_panel(_):
     buttons = [
         [
-            InlineKeyboardButton(
-                text=_["S_B_3"],
-                url=f"https://t.me/{app.username}?startgroup=true",
-            )
+            InlineKeyboardButton(text=_["S_B_3"], url=f"https://t.me/{app.username}?startgroup=true",)
         ],
-
-            InlineKeyboardButton(text=_["S_B_2"], url=config.SUPPORT_CHAT),
+            InlineKeyboardButton(text=_["S_B_5"], user_id=config.OWNER_ID),
             InlineKeyboardButton(text=_["S_B_6"], url=config.SUPPORT_CHANNEL),
         ],
         [

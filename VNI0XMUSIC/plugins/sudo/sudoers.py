b857d8517e5309dca11d2a91c30fa6fde4681670
野买 <madhuripatel509@gmail.com>
2025-07-29 09:05:32 +0530

    Update sudoers.py

--- VNI0XMUSIC/plugins/sudo/sudoers.py
+++ VNI0XMUSIC/plugins/sudo/sudoers.py
@@ -48,7 +48,7 @@ async def userdel(client, message: Message, _):
 @language
 async def sudoers_list(client, message: Message, _):
     if message.from_user.id not in SUDOERS:
-        return await message.reply_text("💔 <b>ᴏᴡɴᴇʀs:</b>\n1➤ <a href='https://t.me/AnimalJanwarrrr'>⛦°𝗔𝗡𝗜𝗠𝗔𝗟°⛦</a>",
+        return await message.reply_text("💔 <b>ᴏᴡɴᴇʀs:</b>\n1➤ <a href='https://t.me/VNI0Xr'>野买</a>",
         disable_web_page_preview=True,
         parse_mode="html")
     text = _["sudo_5"]

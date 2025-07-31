814df6e7e3ab55b4dcca048b5bf76263cef01d50
野买 <madhuripatel509@gmail.com>
2025-07-31 11:05:18 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,8 +1,12 @@
 import os
 from os import getenv
 from dotenv import load_dotenv
+from pyrogram import filters  
 
 load_dotenv()
+
+BANNED_USERS = filters.user()
+
 
 
 # Get this value from my.telegram.org/apps
